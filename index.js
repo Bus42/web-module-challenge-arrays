afd347588882f0bef83a411f398f997c62cc3e1d
Greg Brewton <gbrewton1980@gmail.com>
2021-01-29 17:51:04 -0700

    Completes Stretch 1

--- index.js
+++ index.js
@@ -181,8 +181,10 @@ Use the getAverageWordLength function below to do the following:
     For example: getAverageWordLength(originalFlavors) should return a number between 0 and 3.     
 */
 
-function getAverageWordLength( /*code here*/ ) {
-    /*code here*/
+function getAverageWordLength(wordArray) {
+    const wordLengthArray = wordArray.map(word => word.length);
+    const avgLength = wordLengthArray.reduce((a, b) => a + b) / wordArray.length;
+    return avgLength;
 }
 
 
